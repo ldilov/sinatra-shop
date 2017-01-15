07911a5cde79b69b1bec0bd6cd0ff4c31b7cf74f
Lazar Dilov <ldilov@yahoo.com>
2017-01-15 13:49:47 +0200

    Styling fix

--- db/migrate/20170115104404_create_products.rb
+++ db/migrate/20170115104404_create_products.rb
@@ -2,7 +2,7 @@
 class CreateProducts < ActiveRecord::Migration[5.0]
   def change
     create_table :products do |t|
-      t.float   :price, null: false
+      t.float   :price,    null: false
       t.integer :quantity, null: false
       t.integer :ram
       t.float   :cpu_frequency
