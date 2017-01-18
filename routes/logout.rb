30d54885fac0d02acee7fe5ceb62bab44d231419
Lazar Dilov <ldilov@yahoo.com>
2017-01-18 14:36:13 +0200

    Fixed logout error

--- routes/logout.rb
+++ routes/logout.rb
@@ -1,5 +1,5 @@
 get '/logout' do
-  session[:logged_in].map { |param, value| session[param] = nil}
+  session.map { |param, value| session[param] = nil}
   flash[:error] = nil
   flash[:success] = nil
   redirect '/'
