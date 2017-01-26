2252db7717fd3e13e734dc32043fd092c084de12
Lazar Dilov <ldilov@yahoo.com>
2017-01-26 11:05:35 +0200

    Added profile page

--- app.rb
+++ app.rb
@@ -4,6 +4,7 @@ require 'sinatra/flash'
 
 Dir["#{__dir__}/models/*.rb"].each { |file| require_relative file }
 Dir["#{__dir__}/routes/*.rb"].each { |file| require_relative file }
+Dir["#{__dir__}/routes/*/*.rb"].each { |file| require_relative file }
 
 set :bind, '0.0.0.0'
 set :root, __dir__
---
+++ routes/profile/edit.rb
@@ -0,0 +1,3 @@
+get '/profile/edit' do
+
+end
